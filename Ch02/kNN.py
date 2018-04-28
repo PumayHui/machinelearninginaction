d654d5a75fa2d71a814fb85f30104121a55e856e
PumayHui <13572067484@163.com>
2018-04-28 11:10:40 +0800

    Update kNN.py

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -1,126 +1,163 @@
-'''
-Created on Sep 16, 2010
-kNN: k Nearest Neighbors
-
-Input:      inX: vector to compare to existing dataset (1xN)
-            dataSet: size m data set of known vectors (NxM)
-            labels: data set labels (1xM vector)
-            k: number of neighbors to use for comparison (should be an odd number)
-            
-Output:     the most popular class label
-
-@author: pbharrin
-'''
+# -*- coding: UTF-8 -*-
 from numpy import *
+import numpy as np
 import operator
-from os import listdir
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+import matplotlib.lines as mlines
 
-def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
-    sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()     
-    classCount={}          
-    for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
-
-def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
+def creatDataset():
+    group = array([[1.0,1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0] #get dataset ranks
+    # calculate distance between inX and dataset
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances= sqDistances**0.5
+    # return index of distances by ascending sorted
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    # return the top K most frequently occurring as the current prediction
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    sortedClassCount = sorted(classCount.items(),
+                              key=operator.itemgetter(1),
+                              reverse=True)
+    return sortedClassCount[0][0]
+
 def file2matrix(filename):
-    love_dictionary={'largeDoses':3, 'smallDoses':2, 'didntLike':1}
     fr = open(filename)
     arrayOLines = fr.readlines()
-    numberOfLines = len(arrayOLines)            #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
-    classLabelVector = []                       #prepare labels return   
+    numberOfLines = len(arrayOLines)
+    returnMat = np.zeros((numberOfLines,3))
+    classLabelVector = []
     index = 0
     for line in arrayOLines:
         line = line.strip()
         listFromLine = line.split('\t')
+        # print(line)
         returnMat[index,:] = listFromLine[0:3]
-        if(listFromLine[-1].isdigit()):
-            classLabelVector.append(int(listFromLine[-1]))
-        else:
-            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        if listFromLine[-1] == 'didntLike':
+            classLabelVector.append(1)
+        elif listFromLine[-1] == 'smallDoses':
+            classLabelVector.append(2)
+        elif listFromLine[-1] == 'largeDoses':
+            classLabelVector.append(3)
         index += 1
-    return returnMat,classLabelVector
+    return returnMat, classLabelVector
 
-    
-def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
-    m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m,1))
-    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
-    return normDataSet, ranges, minVals
-   
-def datingClassTest():
-    hoRatio = 0.50      #hold out 10%
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
-    errorCount = 0.0
-    for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
-        if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
-    print errorCount
-    
-def classifyPerson():
-    resultList = ['not at all', 'in small doses', 'in large doses']
-    percentTats = float(raw_input(\
-                                  "percentage of time spent playing video games?"))
-    ffMiles = float(raw_input("frequent flier miles earned per year?"))
-    iceCream = float(raw_input("liters of ice cream consumed per year?"))
-    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    inArr = array([ffMiles, percentTats, iceCream, ])
-    classifierResult = classify0((inArr - \
-                                  minVals)/ranges, normMat, datingLabels, 3)
-    print "You will probably like this person: %s" % resultList[classifierResult - 1]
-    
 def img2vector(filename):
-    returnVect = zeros((1,1024))
+    returnVect = zeros((1, 1024))
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
+            returnVect[0, 32 * i + j] = int(lineStr[j])
     return returnVect
 
-def handwritingClassTest():
-    hwLabels = []
-    trainingFileList = listdir('trainingDigits')           #load the training set
-    m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
-    for i in range(m):
-        fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
-    testFileList = listdir('testDigits')        #iterate through the test set
+def showdatas(datingDataMat, datingLabels):
+    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+    fig, axs = plt.subplots(nrows=2, ncols=2,sharex=False, sharey=False, figsize=(13,8))
+
+    numberOfLabels = len(datingLabels)
+    LabelsColors = []
+    for i in datingLabels:
+        if i == 1:
+            LabelsColors.append('black')
+        if i == 2:
+            LabelsColors.append('orange')
+        if i == 3:
+            LabelsColors.append('red')
+
+    axs[0][0].scatter(x=datingDataMat[:,0], y=datingDataMat[:,1], color=LabelsColors,s=15, alpha=.5)
+
+    axs0_title_text = axs[0][0].set_title('每年获得的飞行常客里程数与玩视频游戏所消耗时间占比',FontProperties=font)
+    axs0_xlabel_text = axs[0][0].set_xlabel('每年获得的飞行常客里程数',FontProperties=font)
+    axs0_ylabel_text = axs[0][0].set_ylabel('玩视频游戏所消耗时间占比',FontProperties=font)
+    plt.setp(axs0_title_text, size=9, weight='bold', color='red')  
+    plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')  
+    plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black') 
+
+
+    axs[0][1].scatter(x=datingDataMat[:,0], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
+
+    axs1_title_text = axs[0][1].set_title('每年获得的飞行常客里程数与每周消费的冰激淋公升数',FontProperties=font)
+    axs1_xlabel_text = axs[0][1].set_xlabel('每年获得的飞行常客里程数',FontProperties=font)
+    axs1_ylabel_text = axs[0][1].set_ylabel('每周消费的冰激淋公升数',FontProperties=font)
+    plt.setp(axs1_title_text, size=9, weight='bold', color='red')  
+    plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')  
+    plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black') 
+
+
+    axs[1][0].scatter(x=datingDataMat[:,1], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
+
+    axs2_title_text = axs[1][0].set_title('玩视频游戏所消耗时间占比与每周消费的冰激淋公升数',FontProperties=font)
+    axs2_xlabel_text = axs[1][0].set_xlabel('玩视频游戏所消耗时间占比',FontProperties=font)
+    axs2_ylabel_text = axs[1][0].set_ylabel('每周消费的冰激淋公升数',FontProperties=font)
+    plt.setp(axs2_title_text, size=9, weight='bold', color='red')  
+    plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')  
+    plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black') 
+
+    didntLike = mlines.Line2D([], [], color='black', marker='.',
+                      markersize=6, label='didntLike')
+    smallDoses = mlines.Line2D([], [], color='orange', marker='.',
+                      markersize=6, label='smallDoses')
+    largeDoses = mlines.Line2D([], [], color='red', marker='.',
+                      markersize=6, label='largeDoses')
+
+    axs[0][0].legend(handles=[didntLike,smallDoses,largeDoses])
+    axs[0][1].legend(handles=[didntLike,smallDoses,largeDoses])
+    axs[1][0].legend(handles=[didntLike,smallDoses,largeDoses])
+
+    # plt.show()
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0) #存放每列的最小值
+    maxVals = dataSet.max(0) #存放每列的最大值
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
     errorCount = 0.0
-    mTest = len(testFileList)
-    for i in range(mTest):
-        fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
-        if (classifierResult != classNumStr): errorCount += 1.0
-    print "\nthe total number of errors is: %d" % errorCount
-    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is : %d"
+              % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]) : errorCount += 1.0
+    print("the total error rate is : %f " % (errorCount/float(numTestVecs)))
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("You will probably like this person: ",
+          resultList[classifierResult - 1])
+
+# def handwritingClassTest():
+
+if __name__ == '__main__':
+    testVector = img2vector('testDigits/1_13.txt')
+    print(testVector[0, :])
+    # classifyPerson()
